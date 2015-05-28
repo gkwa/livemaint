2e411941a768d87c07df8a1c9c18e47e03c39a87
Taylor Monacelli <taylormonacelli@gmail.com>
2015-05-27 23:56:12 -0700

    Revert "For testing only: eliminate permission problems by granting repl user all rights"
    This reverts commit 5bcc2dc5e45d293c12498b08d8cf36a4c069d5f6.

--- tmpl/mysql_master_master_replication_setup.sh
+++ tmpl/mysql_master_master_replication_setup.sh
@@ -193,13 +193,13 @@ SET SQL_SAFE_UPDATES = 0;
 delete from mysql.user WHERE User = 'sls_repl';
 
 -- Add user back in
--- GRANT REPLICATION SLAVE,replication client ON *.* TO '{{mysql_repl_username}}'@'{other_server_ip}' IDENTIFIED BY '{{mysql_repl_pass}}';
--- show grants for '{{mysql_repl_username}}'@'{other_server_ip}';
+GRANT REPLICATION SLAVE,replication client ON *.* TO '{{mysql_repl_username}}'@'{other_server_ip}' IDENTIFIED BY '{{mysql_repl_pass}}';
+show grants for '{{mysql_repl_username}}'@'{other_server_ip}';
 
 -- http://stackoverflow.com/a/5016587/1495086
 -- don't do this in production, but helpful for debug to give full
 -- rights to user:
-GRANT ALL PRIVILEGES ON streambox_live.* TO '{{mysql_repl_username}}'@'%' WITH GRANT OPTION;
+-- GRANT ALL PRIVILEGES ON streambox_live.* TO '{{mysql_repl_username}}'@'%' WITH GRANT OPTION;
 
 flush privileges;
 
