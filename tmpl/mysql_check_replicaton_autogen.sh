6a44c4960b8553f65675400eab5aba5b3dca1953
Taylor Monacelli <taylormonacelli@gmail.com>
2015-06-17 22:53:26 -0700

    yada-yada

--- tmpl/mysql_check_replicaton_autogen.sh
+++ tmpl/mysql_check_replicaton_autogen.sh
@@ -8,9 +8,23 @@ cd /tmp
 cat << __EOT__ >{{scrBase}}.sql
 use {{database}};
 
+SELECT '----------------------------------------------' AS '';
+
+
 -- http://blog.webyog.com/2012/11/20/how-to-monitor-mysql-replication
 -- check slave status
+
 SHOW GLOBAL STATUS like 'slave_running';
+
+SELECT 'SHOW MASTER STATUS' AS '';
+SHOW MASTER STATUS;
+
+SELECT 'SHOW SLAVE STATUS;' AS '';
+SHOW SLAVE STATUS;
+
+
+-- http://stackoverflow.com/a/4225613;
+SELECT TABLE_NAME, ENGINE FROM information_schema.TABLES where TABLE_SCHEMA = 'streambox_live';
 
 __EOT__
 
