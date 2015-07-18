592269f1447a773382e915d195d94b980f92e584
taylormonacelli <tailor@u.washington.edu>
2015-07-18 13:46:20 -0700

    Show all mysql db variables related to character sets and collations

--- tmpl/mysql_check_character_set_autogen.sh
+++ tmpl/mysql_check_character_set_autogen.sh
@@ -7,8 +7,8 @@ trap "rm -f {{scrBase}}.sql; exit" HUP INT QUIT TERM EXIT
 cd /tmp
 cat << __EOT__ >{{scrBase}}.sql
 use {{database}};
-show variables like "character_set_database";
-show variables like "collation_database";
+show variables like "character_set%";
+show variables like "collation%";
 
 __EOT__
 
